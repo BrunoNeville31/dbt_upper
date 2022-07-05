6bc0919d8503b323fa26f311e109b2749831d272
brunoneville31 <brunoneville31@gmail.com>
2022-07-05 17:37:16 -0300

    Ajuste referencia pasta

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,4 +1,4 @@
 
 {{ config(materialized='table') }}
 
-SELECT sum(qtd) as qtd, cpf, sum(valor_total) as valor_total FROM my_first_dbt_model_raw group by cpf
+SELECT sum(qtd) as qtd, cpf, sum(valor_total) as valor_total FROM public.my_first_dbt_model_raw group by cpf
